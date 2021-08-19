a0fc98d653118fe119bc1dc0ed4db46681210ebc
swandavid <dps5610@psu.edu>
2021-08-19 02:27:44 -0400

    updated forgot password and profile

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,22 @@
 import React, { useRef, useState } from "react"
 import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
+import tw from "twin.macro";
+import styled from "styled-components";
+
+import logo from "../images/reconnect-r-logo.jpg";
+
+const Input = tw.input`select-none w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+
+const SubmitButton = styled.button`
+  ${tw`mt-5 tracking-wide font-semibold bg-green-800 text-gray-100 w-full py-4 rounded-lg hover:bg-green-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
+  .icon {
+    ${tw`w-6 h-6 -ml-2`}
+  }
+  .text {
+    ${tw`ml-3 select-none font-display`}
+  }
+`;
 
 export default function ForgotPassword() {
   const emailRef = useRef()
@@ -26,29 +42,36 @@ export default function ForgotPassword() {
   }
 
   return (
-    <>
-      <div>
-        <div>
-          <h2 className="text-center mb-4">Password Reset</h2>
-          {error && <alert variant="danger">{error}</alert>}
-          {message && <alert variant="success">{message}</alert>}
+    <div tw="relative min-h-screen bg-green-800 text-white font-medium flex justify-center">
+      <div tw="max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg grid place-items-center flex-1">
+        <div tw="w-1/2">
+        <Link to="/">
+          <img tw="select-none mx-auto w-1/4" src={logo} />
+        </Link>
+        <div tw="grid place-items-center py-10 font-display font-semibold">
+          <h2 tw="mb-6 text-center select-none text-2xl xl:text-3xl font-extrabold font-display">Password Reset</h2>
           <form onSubmit={handleSubmit}>
-            <div id="email">
-              <h1>Email</h1>
-              <input type="email" ref={emailRef} required />
-            </div>
-            <button disabled={loading} className="w-100" type="submit">
-              Reset Password
-            </button>
+            <Input type="email" placeholder="Email" ref={emailRef} required/>
+            <p tw="text-sm text-red-900 text-center font-display">{error}</p>
+            <p tw="text-sm text-red-900 text-center font-display">{message}</p>
+            <SubmitButton disabled={loading} type="submit">
+              <span className="text">Reset Password</span>
+            </SubmitButton>
           </form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
-          </div>
+          <p tw="mt-6 text-base text-gray-600 text-center">
+            <Link to="/login" tw="select-none border-b border-gray-500 border-dotted">
+              Login
+            </Link>
+          </p>
+          <p tw="mt-4 text-sm text-gray-600 text-center select-none">
+            Need an account?{" "}
+            <Link to="/signup" tw="select-none border-b border-gray-500 border-dotted cursor-pointer">
+              Sign Up
+            </Link>
+          </p>
+        </div>
         </div>
       </div>
-      <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
-      </div>
-    </>
+    </div>
   )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,22 @@
 import React, { useRef, useState } from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 import tw from "twin.macro";
+import styled from "styled-components";
 
-const MainContainer = tw.div``;
+import logo from "../images/reconnect-r-logo.jpg";
+
+const Input = tw.input`select-none w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+
+const SubmitButton = styled.button`
+  ${tw`mt-5 tracking-wide font-semibold bg-green-800 text-gray-100 w-full py-4 rounded-lg hover:bg-green-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
+  .icon {
+    ${tw`w-6 h-6 -ml-2`}
+  }
+  .text {
+    ${tw`ml-3 select-none font-display`}
+  }
+`;
 
 export default function Profile() {
   const emailRef = useRef()
@@ -34,7 +46,7 @@ export default function Profile() {
 
     Promise.all(promises)
       .then(() => {
-        history.push("/")
+        history.push("/home")
       })
       .catch(() => {
         setError("Failed to update account")
@@ -45,46 +57,31 @@ export default function Profile() {
   }
 
   return (
-    <MainContainer>
-      <Card>
-        <Card.Body>
-          <h2 tw="text-center mb-4">Update Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control
-                type="email"
-                ref={emailRef}
-                required
-                defaultValue={currentUser.email}
-              />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control
-                type="password"
-                ref={passwordConfirmRef}
-                placeholder="Leave blank to keep the same"
-              />
-            </Form.Group>
-            <Button disabled={loading} tw="w-144" type="submit">
-              Update
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div tw="w-144 text-center mt-2">
-        <Link to="/home">Cancel</Link>
+    <div tw="relative min-h-screen bg-green-800 text-white font-medium flex justify-center">
+      <div tw="max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg grid place-items-center flex-1">
+        <div tw="w-1/2">
+          <Link to="/home">
+            <img tw="select-none mx-auto w-1/4" src={logo} />
+          </Link>
+          <div tw="grid place-items-center py-10 font-display font-semibold">
+            <h2 tw="mb-6 text-center select-none text-2xl xl:text-3xl font-extrabold font-display">Update Profile</h2>
+            <form onSubmit={handleSubmit}>
+              <Input type="email" placeholder="Email" ref={emailRef} defaultValue={currentUser.email} required/>
+              <Input type="password" placeholder="Leave blank to keep the same" ref={passwordRef}/>
+              <Input type="password" placeholder="Leave blank to keep the same" ref={passwordConfirmRef}/>
+              <p tw="text-sm text-red-900 text-center font-display h-10 mt-4">{error}</p>
+              <SubmitButton disabled={loading} type="submit">
+                <span className="text">Update</span>
+              </SubmitButton>
+            </form>
+            <p tw="mt-6 text-base text-gray-600 text-center">
+              <Link to="/home" tw="select-none border-b border-gray-500 border-dotted">
+                Cancel
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
-    </MainContainer>
+    </div>
   )
 }
