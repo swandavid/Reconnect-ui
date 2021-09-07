8f562a84db9b56bf3d09a0c398acf92b7b286597
David Swan <dps5610@psu.edu>
2021-09-07 13:01:57 -0400

    prepping for spaceship gif layout

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -2,18 +2,16 @@ import React from 'react';
 import tw from "twin.macro";
 import Video from "../videos/reconnect-gif.mp4"
 
-const MainContainer = tw.div`h-screen w-full bg-green-700 grid place-items-center relative z-20`;
+const MainContainer = tw.div`h-screen py-20 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 place-items-center`;
 
 export default function MainLanding() {
     return(
         <MainContainer>
-            <div tw="absolute w-full h-full overflow-hidden">
-                <video tw="object-cover w-full h-full" autoPlay loop muted src={Video} type="video/mp4"/>
+            <div tw="bg-gray-200 mt-10 w-1/2 h-3/4">
+                Spaceship Gifs
             </div>
-            <div tw="z-30 w-full p-10 md:w-1/2 xl:w-1/4 absolute grid items-center bg-white">
-                <h1 tw="text-4xl font-display font-bold text-center text-green-800 place-self-start">Reimagine</h1>
-                <h1 tw="text-4xl font-display font-bold text-center text-green-800 place-self-center">Reinvent</h1>
-                <h1 tw="text-4xl font-display font-bold text-center text-green-800 place-self-end">Reconnect</h1>
+            <div tw="bg-gray-400">
+                <h1 tw="font-display font-semibold w-1/2 h-3/4">"Mission Statement"</h1>
             </div>
         </MainContainer>
     );
