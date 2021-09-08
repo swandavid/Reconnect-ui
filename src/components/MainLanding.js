4ebb2a491d04bf5d3868e7248508e20e2126ec50
swandavid <dps5610@psu.edu>
2021-09-08 00:08:09 -0400

    updated landing ui according to changes

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import tw from "twin.macro";
-import Video from "../videos/reconnect-gif.mp4"
+import SpaceShip from "../images/landing-spaceship.svg";
 
 const MainContainer = tw.div`h-screen py-20 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 place-items-center`;
 
 export default function MainLanding() {
     return(
         <MainContainer>
-            <div tw="bg-gray-200 mt-10 w-1/2 h-3/4">
-                Spaceship Gifs
+            <div tw="mt-10 w-full lg:w-1/2 h-3/4">
+                <img src={SpaceShip}></img>
             </div>
-            <div tw="bg-gray-400">
-                <h1 tw="font-display font-semibold w-1/2 h-3/4">"Mission Statement"</h1>
+            <div tw="w-2/3 h-1/3 text-center">
+                <p tw="font-display font-semibold w-full h-full text-2xl">"Mission Statement"</p>
             </div>
         </MainContainer>
     );
