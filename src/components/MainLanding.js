1c3b3614067069a8ce40b95765f2a4b058d18f5c
David Swan <dps5610@psu.edu>
2021-09-08 23:02:39 -0400

    testing main landing mobile

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -2,7 +2,7 @@ import React from 'react';
 import tw from "twin.macro";
 import SpaceShip from "../images/landing-spaceship.svg";
 
-const MainContainer = tw.div`h-screen my-12 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 md:place-items-center items-center`;
+const MainContainer = tw.div`my-12 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 md:place-items-center items-center`;
 
 export default function MainLanding() {
     return(
@@ -10,7 +10,7 @@ export default function MainLanding() {
             <div tw="mt-10 w-3/4">
                 <img src={SpaceShip}></img>
             </div>
-            <div tw="w-2/3 h-1/4 grid text-center">
+            <div tw="w-2/3 grid text-center">
                 <h1 tw="font-yeseva font-black md:font-bold w-full h-full text-4xl md:text-6xl text-yellow-400">Scroll Less</h1>
                 <h1 tw="font-yeseva font-black md:font-bold w-full h-full text-4xl md:text-6xl text-yellow-400">Smile More</h1>
             </div>
