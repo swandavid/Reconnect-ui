fd9eb267b025b8ed884d3094800c6f15f3409af8
David Swan <dps5610@psu.edu>
2021-09-08 22:52:41 -0400

    changed main landing text

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -2,17 +2,17 @@ import React from 'react';
 import tw from "twin.macro";
 import SpaceShip from "../images/landing-spaceship.svg";
 
-const MainContainer = tw.div`h-screen py-20 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 place-items-center`;
+const MainContainer = tw.div`h-screen my-20 w-full bg-blue-200 flex flex-col md:grid md:grid-cols-2 md:place-items-center items-center`;
 
 export default function MainLanding() {
     return(
         <MainContainer>
-            <div tw="mt-10 w-3/4 h-3/4">
+            <div tw="mt-10 w-3/4">
                 <img src={SpaceShip}></img>
             </div>
             <div tw="w-2/3 h-1/4 grid text-center">
-                <h1 tw="font-yeseva font-semibold w-full h-full text-4xl text-yellow-400">Scroll Less</h1>
-                <h1 tw="font-yeseva font-semibold w-full h-full text-4xl text-yellow-400">Smile More</h1>
+                <h1 tw="font-yeseva font-bold w-full h-full text-4xl md:text-6xl text-yellow-400">Scroll Less</h1>
+                <h1 tw="font-yeseva font-bold w-full h-full text-4xl md:text-6xl text-yellow-400">Smile More</h1>
             </div>
         </MainContainer>
     );
