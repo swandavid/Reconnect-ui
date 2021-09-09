060d529dd0005c5886a910fc28567039169368fc
David Swan <dps5610@psu.edu>
2021-09-08 22:42:55 -0400

    changes to main landing text and image height

--- src/components/MainLanding.js
+++ src/components/MainLanding.js
@@ -7,12 +7,12 @@ const MainContainer = tw.div`h-screen py-20 w-full bg-blue-200 flex flex-col md:
 export default function MainLanding() {
     return(
         <MainContainer>
-            <div tw="mt-10 w-1/2 h-3/4">
+            <div tw="mt-10 w-3/4 h-3/4">
                 <img src={SpaceShip}></img>
             </div>
             <div tw="w-2/3 h-1/4 grid text-center">
-                <h1 tw="font-display font-bold w-full h-full text-4xl text-yellow-400">Scroll Less</h1>
-                <h1 tw="font-display font-bold w-full h-full text-4xl text-yellow-400">Smile More</h1>
+                <h1 tw="font-yeseva font-semibold w-full h-full text-4xl text-yellow-400">Scroll Less</h1>
+                <h1 tw="font-yeseva font-semibold w-full h-full text-4xl text-yellow-400">Smile More</h1>
             </div>
         </MainContainer>
     );
